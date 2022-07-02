f13c02f968de284cf291f247e601d331cb21fbf9
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-02 20:24:39 +0100

    login front end

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <>
+        <div className="w-full h-screen ">
+            <img className="hidden sm:block absolute w-full h-full object-cover" src ="https://assets.nflxext.com/ffe/siteui/vlv3/271ac55e-7228-438e-824e-92db37981e59/8934e728-0aea-4211-b63f-a0b7819e1f1b/GB-en-20220627-popsignuptwoweeks-perspective_alpha_website_small.jpg"/>
+            <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+            <div className="fixed w-full px-4 py-24 z-50">
+                <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+                    <div className="max-w-[320px] mx-auto py-16">
+                        <h1 className="text-3xl font-bold"> Sign In</h1>
+                        <form className="w-full flex flex-col py-4"> 
+                            <input className="py-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete='email'/>
+                            <input className="py-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete='current-password'/>
+                            <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign In</button>
+                            <div className="flex justify-between items-center text-sm text-gray-600">
+                                <p><input className="mr-2" type="checkbox"  /> Remember Me </p>
+                                <p> Need Help?</p>
+                            </div>
+                            <p className="py-8">
+                                <span className="text-gray-600"> 
+                                    New to Netflix?
+                                </span>{' '}
+                                <Link to="/signup">Sign Up</Link>
+                            </p>
+    
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+        </>
+      );
 }
 
 export default Login
