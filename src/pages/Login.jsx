511cfdddebfaa18c39dc1cb1dd5db6eba102c6f7
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-02 21:28:46 +0100

    working login/logout funcitopn with firebase back-end

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
+import { UserAuth} from "../context/AuthContext";
 
 const Login = () => {
     return (
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {UserAuth} from "../context/AuthContext";
 
 const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const {user, signUp} = UserAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await signUp(email, password);
+            navigate("/")
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
     <>
     <div className="w-full h-screen ">
-        <img className="hidden sm:block absolute w-full h-full object-cover" src ="https://assets.nflxext.com/ffe/siteui/vlv3/271ac55e-7228-438e-824e-92db37981e59/8934e728-0aea-4211-b63f-a0b7819e1f1b/GB-en-20220627-popsignuptwoweeks-perspective_alpha_website_small.jpg"/>
+        <img className="hidden sm:block absolute w-full h-full object-cover" 
+            src ="https://assets.nflxext.com/ffe/siteui/vlv3/271ac55e-7228-438e-824e-92db37981e59/8934e728-0aea-4211-b63f-a0b7819e1f1b/GB-en-20220627-popsignuptwoweeks-perspective_alpha_website_small.jpg"/>
         <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
         <div className="fixed w-full px-4 py-24 z-50">
             <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
                 <div className="max-w-[320px] mx-auto py-16">
                     <h1 className="text-3xl font-bold">Sign Up</h1>
-                    <form className="w-full flex flex-col py-4"> 
-                        <input className="py-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete='email'/>
-                        <input className="py-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete='current-password'/>
+                    <form onSubmit={handleSubmit} className="w-full flex flex-col py-4"> 
+                        <input onChange={(e) => setEmail(e.target.value)} className="py-3 my-2 bg-gray-700 rounded" type="email" placeholder="Email" autoComplete='email'/>
+                        <input onChange={(e) => setPassword(e.target.value)} className="py-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete='current-password'/>
                         <button className="bg-red-600 py-3 my-6 rounded font-bold">Sign Up</button>
                         <div className="flex justify-between items-center text-sm text-gray-600">
                             <p><input className="mr-2" type="checkbox"  /> Remember Me </p>
